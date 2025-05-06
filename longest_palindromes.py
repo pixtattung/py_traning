a2ccb74999590e86bf38780642fd9defeb047d44
pixtattung <tung.trieuthanh@pixta.co.jp>
2025-05-06 17:04:09 +0700

    add

---
+++ longest_palindromes.py
@@ -0,0 +1,15 @@
+def longest(s):
+    dp = [[False]*len(s) for _ in range(len(s)) ]
+    for i in range(len(s)):
+        dp[i][i]=True
+    ans=s[0]
+    for j in range(len(s)):
+        for i in range(j):
+            if s[i]==s[j] and (dp[i+1][j-1] or j==i+1):
+                dp[i][j]=True
+                if j-i+1>len(ans):
+                    ans=s[i:j+1]
+    return ans
+
+# s = "babad"
+# print(longest(s))
